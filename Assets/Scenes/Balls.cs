40491580e1d6641c48936238b4c0e78d3e942293
akhemi1207 <57777237+akhemi1207@users.noreply.github.com>
2020-03-07 11:33:08 -0500

    cañon
    Se crea el cañon con texturas pero no dispara de forma adecuada.

---
+++ Assets/Scenes/Balls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    Rigidbody sphereRig;
+
+    void Update()
+    {
+        sphereRig = GetComponent<Rigidbody>();
+        sphereRig.AddForce(transform.forward * 50);
+    }
+
+}
---
+++ Assets/Scenes/Movimiento.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public Transform pivot;
+    public Transform target;
+    public GameObject ball;
+
+    public float gyroSpeed = 1;
+    float xDir;
+    float yDir;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(ball, target.position, target.rotation);
+        }
+        xDir = Input.GetAxis("Horizontal");
+        yDir = Input.GetAxis("Vertical");
+
+        if (xDir>0) // rota derecha
+        {
+            transform.Rotate(0, gyroSpeed, 0);
+        }
+        else if (xDir<0) // rota izquierda
+        {
+            transform.Rotate(0, -gyroSpeed, 0);
+        }
+
+        if (yDir>0)
+        {
+            pivot.Rotate(0, gyroSpeed / 2,0 );
+        }
+        else if (yDir<0)
+        {
+            pivot.Rotate(0, -gyroSpeed / 2, 0);
+
+        }
+    }
+
+}
